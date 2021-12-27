071fdbb90cfb888572513ca5f6b8753e61baaef0
Tomsawyerhu <1095549886@qq.com>
2021-12-27 16:16:54 +0800

    添加图标

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # jacoconut-front
 Jacoconut's frontend，gradle-built intellij plugin，written in Swing &amp; Java.
+![](./public/logo2.png)
---
+++ src/test/java/Test2.java
@@ -0,0 +1,52 @@
+public class Test2 {
+    @org.junit.Test
+    public void nothingTest1(){
+        System.out.println(1);
+    }
+    @org.junit.Test
+    public void nothingTest2(){
+        System.out.println(2);
+        System.out.println(2);
+    }
+    @org.junit.Test
+    public void nothingTest3(){
+        System.out.println(3);
+        System.out.println(3);
+        System.out.println(3);
+    }
+    @org.junit.Test
+    public void nothingTest4(){
+        System.out.println(4);
+        System.out.println(4);
+        System.out.println(4);
+        System.out.println(4);
+    }
+    @org.junit.Test
+    public void nothingTest5(){
+        System.out.println(5);
+        System.out.println(5);
+        System.out.println(5);
+        System.out.println(5);
+        System.out.println(5);
+    }
+    @org.junit.Test
+    public void nothingTest6(){
+        System.out.println(6);
+        System.out.println(6);
+        System.out.println(6);
+        System.out.println(6);
+        System.out.println(6);
+        System.out.println(6);
+    }
+    @org.junit.Test
+    public void nothingTest7(){
+        System.out.println(7);
+        System.out.println(7);
+        System.out.println(7);
+        System.out.println(7);
+        System.out.println(7);
+        System.out.println(7);
+        System.out.println(7);
+    }
+
+}
